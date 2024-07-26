41df6511e7fa0bdd7dfc8782654eae1ade7d9dcf
Abhishek Dave <abhishek.dave@carbon-minds.com>
2024-07-26 18:25:55 +0200

    Update user login with hash password logic correctly on /auth/login

--- src/models/user_model.py
+++ src/models/user_model.py
@@ -13,7 +13,7 @@ class User(db.Model):
         self.password = generate_password_hash(password)
 
     def check_password(self, password):
-        return self.password == password
+        return check_password_hash(self.password, password)
 
 
 
